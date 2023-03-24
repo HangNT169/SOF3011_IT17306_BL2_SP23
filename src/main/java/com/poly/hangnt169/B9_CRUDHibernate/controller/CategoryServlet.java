e57863aea9f6b3010aadd8339484612a584fa836
HangNT169 <HangNT169@fpt.edu.vn>
2023-03-24 14:31:13 +0700

    B9: CRUD Full Hibernate

---
+++ src/main/java/com/poly/hangnt169/B9_CRUDHibernate/controller/CategoryServlet.java
@@ -0,0 +1,23 @@
+package com.poly.hangnt169.B9_CRUDHibernate.controller;
+/**
+ * @author hangnt169
+ */
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "CategoryServlet", value = "/CategoryServlet")
+public class CategoryServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
